8561322566575f9daad914f60cdca6a8f20fd48f
G.Nivedhitha <g.nivedhitha@Nivs-MacBook-Air.local>
2024-07-04 14:23:15 +0530

    socket URL has been changed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5001", {
+			const socket = io("https://chat-app-using-mern-a3bs.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
